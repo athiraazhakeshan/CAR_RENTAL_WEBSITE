8f2bd62b2f66513aa2be97f4862665a3475ad898
athiraazhakeshan <athiraazhakeshan@gmail.com>
2024-12-29 19:09:14 +0530

    a

--- server/index.js
+++ server/index.js
@@ -149,7 +149,7 @@ const startServer = async () => {
     app.use((req, res, next) => {
         const origin = req.headers.origin;
         if (allowedOrigins.includes(origin)) {
-            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Origin",origin);
         }
         res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Accept");
         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
