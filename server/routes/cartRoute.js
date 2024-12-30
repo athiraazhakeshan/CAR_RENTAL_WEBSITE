189b207d18cf6a2a843472c85cc3c4cdd6844c17
athiraazhakeshan <athiraazhakeshan@gmail.com>
2024-12-30 14:03:49 +0530

    cart updated

--- server/routes/cartRoute.js
+++ server/routes/cartRoute.js
@@ -1,12 +1,12 @@
 import e from "express";
 import { authUser } from "../middlewares/authUser.js";
-import { addCarToCart, getCart, getCartItems, removeCarFromCart,clearCart } from "../controllers/cartController.js";
+import { addCarToCart, getCart, getCartItems, removeCarFromCart } from "../controllers/cartController.js";
 const router = e.Router();
 
 router.post("/add-to-cart",authUser,addCarToCart);
 router.get("/get-cart", authUser, getCart);
 router.get("/get-cartItems", authUser, getCartItems);
 router.delete("/remove-car",authUser, removeCarFromCart);
-router.delete("/clear-cart", authUser,clearCart);
+
 
 export { router as cartRouter };
