d5aa936c63e54bc1419064b45bcd489690da8646
athiraazhakeshan <athiraazhakeshan@gmail.com>
2025-01-09 18:01:18 +0530

    change

--- frontend/src/config/axiosInstance.jsx
+++ frontend/src/config/axiosInstance.jsx
@@ -3,10 +3,10 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
 const url = "https://car-rental-website-server-six.vercel.app";
 export const axiosInstance = axios.create({
- // baseURL: `${API_URL}/api`,
+  //baseURL: `${API_URL}/api`,
    baseURL:`${url}/api`,
     withCredentials: true,
     headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json' 
      }
 });
