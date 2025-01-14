b4518cca358a524be0156f145d8fcc8e3e523446
athiraazhakeshan <athiraazhakeshan@gmail.com>
2025-01-14 12:14:56 +0530

    carpicture

--- frontend/src/pages/adminpages/UpdateCarForm.jsx
+++ frontend/src/pages/adminpages/UpdateCarForm.jsx
@@ -1,13 +1,155 @@
+// import React, { useState, useEffect } from 'react';
+// import { useParams } from 'react-router-dom';
+// import { Box, Input, Button, FormControl, FormLabel, Heading, Flex } from '@chakra-ui/react';
+// import axios from 'axios';
+// import { useNavigate } from 'react-router-dom';
+// import { axiosInstance } from '../../config/axiosInstance';
+// const UpdateCarForm = () => {
+//     const navigate=useNavigate();
+//     const { carId } = useParams();
+//     const [car, setCar] = useState(null);
+//     const [formData, setFormData] = useState({
+//         carName: '',
+//         carModel: '',
+//         carCompany: '',
+//         carCategory: '',
+//         carEngine: '',
+//         transmission: '',
+//         carMileage: '',
+//         carFuelType: '',
+//         rentalPriceCharge: '',
+//         carPicture: null,
+//     });
+    
+//     useEffect(() => {
+//         const fetchCar = async () => {
+//             try {
+//                 const res = await axiosInstance.get(`/admin/getcarbyid/${carId}`);
+//                 const data = res.data.data; // This should be the car data
+//                 console.log(data)
+    
+//                 // Check if the data is an array and access the first item if necessary
+//                 if (Array.isArray(data) && data.length > 0) {
+//                     setCar(data[0]); // Assuming data is an array, use the first element
+//                 } else if (data) {
+//                     setCar(data); // If the data is not an array, it should be an object
+//                 } else {
+//                     console.error("No car data found in response");
+//                 }
+    
+//                 // Set form data only after setting the car object
+//                 if (data && !Array.isArray(data)) {
+//                     setFormData({
+//                         carName: data.carName || '',
+//                         carModel: data.carModel || '',
+//                         carCompany: data.carCompany || '',
+//                         carCategory: data.carCategory || '',
+//                         carEngine: data.carEngine || '',
+//                         transmission: data.transmission || '',
+//                         carMileage: data.carMileage || '',
+//                         carFuelType: data.carFuelType || '',
+//                         rentalPriceCharge: data.rentalPriceCharge || '',
+//                         carPicture: null, 
+//                     });
+//                 }
+    
+//             } catch (error) {
+//                 console.error(error);
+//             }
+//         };
+    
+//         fetchCar();
+//     }, [carId]);
+    
+
+//     const handleChange = (e) => {
+//         setFormData({
+//             ...formData,
+//             [e.target.name]: e.target.value,
+//         });
+//     };
+//     const handleFileChange = (e) => {
+//         setFormData({
+//             ...formData,
+//             carPicture: e.target.files[0],
+//         });
+//     };
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
+//         try {
+//             await axiosInstance.patch(`/admin/updatecar/${carId}`, formData);
+//             alert('Car updated successfully');
+//             navigate(`/admin/carlist/${carId}`);
+//         } catch (error) {
+//             console.log(error);
+//         }
+//     };
+
+//     if (!car) {
+//         return <Box>Loading...</Box>;
+//     }
+
+//     return (
+//         <Flex direction="column" align="center" p="4">
+//             <Heading as="h1" mb="4">Update Car Details</Heading>
+//             <Box as="form" onSubmit={handleSubmit} w="100%" maxW="600px">
+//                 <FormControl mb="4">
+//                     <FormLabel>Car Name</FormLabel>
+//                     <Input name="carName" value={formData.carName} onChange={handleChange} />
+//                 </FormControl>
+//                 <FormControl mb="4">
+//                     <FormLabel>Car Model</FormLabel>
+//                     <Input name="carModel" value={formData.carModel} onChange={handleChange} />
+//                 </FormControl>
+//                 <FormControl mb="4">
+//                     <FormLabel>Car Company</FormLabel>
+//                     <Input name="carCompany" value={formData.carCompany} onChange={handleChange} />
+//                 </FormControl>
+//                 <FormControl mb="4">
+//                     <FormLabel>Car Category</FormLabel>
+//                     <Input name="carCategory" value={formData.carCategory} onChange={handleChange} />
+//                 </FormControl>
+//                 <FormControl mb="4">
+//                     <FormLabel>Car Engine</FormLabel>
+//                     <Input name="carEngine" value={formData.carEngine} onChange={handleChange} />
+//                 </FormControl>
+//                 <FormControl mb="4">
+//                     <FormLabel>Transmission</FormLabel>
+//                     <Input name="transmission" value={formData.transmission} onChange={handleChange} />
+//                 </FormControl>
+//                 <FormControl mb="4">
+//                     <FormLabel>Car Mileage</FormLabel>
+//                     <Input name="carMileage" value={formData.carMileage} onChange={handleChange} />
+//                 </FormControl>
+//                 <FormControl mb="4">
+//                     <FormLabel>Fuel Type</FormLabel>
+//                     <Input name="carFuelType" value={formData.carFuelType} onChange={handleChange} />
+//                 </FormControl>
+//                 <FormControl mb="4">
+//                     <FormLabel>Rental Price Charge</FormLabel>
+//                     <Input name="rentalPriceCharge" value={formData.rentalPriceCharge} onChange={handleChange} />
+//                 </FormControl>
+//                 <FormControl mb="4">
+//                     <FormLabel>Car Picture</FormLabel>
+//                     <Input type="file" name="carPicture" accept="image/*" onChange={handleFileChange} />
+//                 </FormControl>
+                
+//                 <Button type="submit" colorScheme="blue">Update</Button>
+//             </Box>
+//         </Flex>
+//     );
+// };
+
+// export default UpdateCarForm;
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box, Input, Button, FormControl, FormLabel, Heading, Flex } from '@chakra-ui/react';
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { axiosInstance } from '../../config/axiosInstance';
+
 const UpdateCarForm = () => {
-    const navigate=useNavigate();
+    const navigate = useNavigate();
     const { carId } = useParams();
-    const [car, setCar] = useState(null);
     const [formData, setFormData] = useState({
         carName: '',
         carModel: '',
@@ -18,27 +160,16 @@ const UpdateCarForm = () => {
         carMileage: '',
         carFuelType: '',
         rentalPriceCharge: '',
-        carPicture: '',
+        carPicture: null,
     });
-    
+
     useEffect(() => {
         const fetchCar = async () => {
             try {
                 const res = await axiosInstance.get(`/admin/getcarbyid/${carId}`);
-                const data = res.data.data; // This should be the car data
-                console.log(data)
-    
-                // Check if the data is an array and access the first item if necessary
-                if (Array.isArray(data) && data.length > 0) {
-                    setCar(data[0]); // Assuming data is an array, use the first element
-                } else if (data) {
-                    setCar(data); // If the data is not an array, it should be an object
-                } else {
-                    console.error("No car data found in response");
-                }
-    
-                // Set form data only after setting the car object
-                if (data && !Array.isArray(data)) {
+                const data = res.data.data;
+
+                if (data) {
                     setFormData({
                         carName: data.carName || '',
                         carModel: data.carModel || '',
@@ -49,18 +180,16 @@ const UpdateCarForm = () => {
                         carMileage: data.carMileage || '',
                         carFuelType: data.carFuelType || '',
                         rentalPriceCharge: data.rentalPriceCharge || '',
-                        carPicture: data.carPicture || '',
+                        carPicture: null,
                     });
                 }
-    
             } catch (error) {
                 console.error(error);
             }
         };
-    
+
         fetchCar();
     }, [carId]);
-    
 
     const handleChange = (e) => {
         setFormData({
@@ -69,18 +198,36 @@ const UpdateCarForm = () => {
         });
     };
 
+    const handleFileChange = (e) => {
+        setFormData({
+            ...formData,
+            carPicture: e.target.files[0],
+        });
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            await axiosInstance.patch(`/admin/updatecar/${carId}`, formData);
+            const data = new FormData();
+            Object.keys(formData).forEach((key) => {
+                if (formData[key]) {
+                    data.append(key, formData[key]);
+                }
+            });
+
+            await axiosInstance.patch(`/admin/updatecar/${carId}`, data, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
             alert('Car updated successfully');
             navigate(`/admin/carlist/${carId}`);
         } catch (error) {
-            console.log(error);
+            console.error(error);
         }
     };
 
-    if (!car) {
+    if (!formData) {
         return <Box>Loading...</Box>;
     }
 
@@ -124,7 +271,10 @@ const UpdateCarForm = () => {
                     <FormLabel>Rental Price Charge</FormLabel>
                     <Input name="rentalPriceCharge" value={formData.rentalPriceCharge} onChange={handleChange} />
                 </FormControl>
-                
+                <FormControl mb="4">
+                    <FormLabel>Car Picture</FormLabel>
+                    <Input type="file" name="carPicture" accept="image/*" onChange={handleFileChange} />
+                </FormControl>
                 <Button type="submit" colorScheme="blue">Update</Button>
             </Box>
         </Flex>
--- server/controllers/carController.js
+++ server/controllers/carController.js
@@ -117,45 +117,85 @@ export const createCar = async (req, res) => {
 
 
     //update 
+    export const updateCar = async (req, res) => {
+      try {
+          const id = req.params.id;
+          const { carName, carModel, transmission, carCompany, carCategory, carEngine, carMileage, carFuelType, rentalPriceCharge } = req.body;
   
-export const updateCar = async (req, res) => {
-  const id = req.params.id;
-    console.log("updated")
-  console.log(id)
-      const body = req.body;
-      console.log(body, "body");
-      let imageUrl;
-      // const { city } = office;
-  const {carName, carModel,transmission, carCompany,carPicture,carCategory,carEngine,carMileage, carSeatCapacity, carFuelType,rentalPriceCharge,office}=req.body;
-  // const findlocation = await OfficeLocation.findOne({ city:office });
-  // console.log(findlocation)
-  //         if (!findlocation) {
-  //           return res.send("please add instructor first");
-  //         }
-  // console.log('image====',req.file);
-
-  if(req.file){
-    const cloudinaryRes=await cloudinaryInstance.uploader.upload(req.file.path)
-    imageUrl=cloudinaryRes.url;
-   //imageUrl=await handleImageUpload(req.file.path)
-  }
-
-console.log(imageUrl,"===imagrUrl");  
-  const updatedCar = await Car.findByIdAndUpdate(
-      { _id: id },
-      { carName, carModel,transmission, carCompany,carPicture:imageUrl, carCategory,carEngine,carMileage, carSeatCapacity, carFuelType,rentalPriceCharge},
-      {
-        new: true,
+          let imageUrl = null;
+  
+          // Handle image upload if file exists
+          if (req.file) {
+              const cloudinaryRes = await cloudinaryInstance.uploader.upload(req.file.path);
+              imageUrl = cloudinaryRes.url;
+          }
+  
+          // Update car details
+          const updatedCar = await Car.findByIdAndUpdate(
+              { _id: id },
+              {
+                  carName,
+                  carModel,
+                  transmission,
+                  carCompany,
+                  carCategory,
+                  carEngine,
+                  carMileage,
+                  carFuelType,
+                  rentalPriceCharge,
+                  ...(imageUrl && { carPicture: imageUrl }),
+              },
+              { new: true }
+          );
+  
+          if (!updatedCar) {
+              return res.status(404).json({ message: "Car not found or update failed" });
+          }
+  
+          res.status(200).json({ message: "Car updated successfully", data: updatedCar });
+      } catch (error) {
+          console.error(error);
+          res.status(500).json({ message: "Internal server error", error });
       }
-    );
+  };
+// export const updateCar = async (req, res) => {
+//   const id = req.params.id;
+//     console.log("updated")
+//   console.log(id)
+//       const body = req.body;
+//       console.log(body, "body");
+//       let imageUrl;
+//       // const { city } = office;
+//   const {carName, carModel,transmission, carCompany,carPicture,carCategory,carEngine,carMileage, carSeatCapacity, carFuelType,rentalPriceCharge,office}=req.body;
+//   // const findlocation = await OfficeLocation.findOne({ city:office });
+//   // console.log(findlocation)
+//   //         if (!findlocation) {
+//   //           return res.send("please add instructor first");
+//   //         }
+//   // console.log('image====',req.file);
+
+//   if(req.file){
+//     const cloudinaryRes=await cloudinaryInstance.uploader.upload(req.file.path)
+//     imageUrl=cloudinaryRes.url;
+//    //imageUrl=await handleImageUpload(req.file.path)
+//   }
+
+// console.log(imageUrl,"===imagrUrl");  
+//   const updatedCar = await Car.findByIdAndUpdate(
+//       { _id: id },
+//       { carName, carModel,transmission, carCompany,carPicture:imageUrl, carCategory,carEngine,carMileage, carSeatCapacity, carFuelType,rentalPriceCharge},
+//       {
+//         new: true,
+//       }
+//     );
   
-    if (!updatedCar) {
-      return res.send("Car is not updated");
-    }
-    console.log(updatedCar);
-    return res.send({data:updatedCar,message:"car is updated"});
+//     if (!updatedCar) {
+//       return res.send("Car is not updated");
+//     }
+//     console.log(updatedCar);
+//     return res.send({data:updatedCar,message:"car is updated"});
     
- };
+//  };
 
 
   //delete
